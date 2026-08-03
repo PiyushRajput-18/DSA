3a9ac37659de1b1f370806bd4ba2d0808ed68413
Piyush Rajput <rajputshikhar776@gmail.com>
2026-08-03 23:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.36%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-int dp[101];
-int solve(int i,vector<int>&nums){
-    if(i>=nums.size()){
-        return 0;
-    }
-    if(dp[i]!=-1){
-        return dp[i];
-    }
-    int steal = nums[i]+solve(i+2,nums);
-    int skip = solve(i+1,nums);
-    return dp[i]=max(steal,skip);
-}
+// int dp[101];
+// int solve(int i,vector<int>&nums){
+//     if(i>=nums.size()){
+//         return 0;
+//     }
+//     if(dp[i]!=-1){
+//         return dp[i];
+//     }
+//     int steal = nums[i]+solve(i+2,nums);
+//     int skip = solve(i+1,nums);
+//     return dp[i]=max(steal,skip);
+// }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
+        // int n = nums.size();
+        // int i=0;
+        // int maxprofit = solve(i,nums);
+        // return maxprofit;  
         int n = nums.size();
-        int i=0;
-        int maxprofit = solve(i,nums);
-        return maxprofit;    
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+        int steal = nums[i-1]+dp[i-2];
+        int skip = dp[i-1];
+        dp[i]=max(steal,skip);
+        }
+        return dp[n];
     }
 };
