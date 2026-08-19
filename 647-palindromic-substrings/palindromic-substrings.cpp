54a6ee8ca1dcdced7bdf132c5d50066bac17de25
Piyush Rajput <rajputshikhar776@gmail.com>
2026-08-19 22:39:28 +0530

    Time: 33 ms (28.10%) | Memory: 11 MB (46.51%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
+     vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+      int count =0;
+      for(int l =1;l<=n;l++){
+         for(int i=0;i+l-1<n;i++){
+            int j = i+l-1;
+            if(i==j){
+                dp[i][j]=true;
+              
+            }
+       else if((i+1)==j){
+                dp[i][j]=s[i]==s[j];
+               
+            }
+            else {
+                dp[i][j]=(s[i]==s[j])&&dp[i+1][j-1];
+            }
+            if(dp[i][j]==true){
+                count++;
+            }
+         }
+      }
+      return count;
+    }
+};
